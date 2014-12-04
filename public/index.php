9ff9d721938f9b75ef4a254252a5bc625a8264c0
fiveflip <fiveflip@gmail.com>
2014-12-04 15:34:02 -0800

    html+css 1
    modified structure to reflect working/public sections, added css/html so
    far

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!-- 
+	Author: sebastian yong, derek moore
+	purpose: bcitcomp 1950 final project
+	page: index.php
+	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+	<head>
+		<title>comp 1950 | bcit</title>
+		<?php include 'includes/head.inc';?>
+	</head>
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
+	<body class="bac3">
+		<div id="wrapper_body">
+			<header class="bac1">
+				<?php include 'includes/header.inc';?>
+			</header>
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
+			<nav>	
+				<?php include 'includes/nav.inc';?>
+			</nav>
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->		
+			<main class="bac1">
+				<?php include 'includes/main.inc';?>
+			</main>
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->		
+			<footer>
+				<?php include 'includes/footer.inc';?>
+			</footer>
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->			
+		</div>
+		
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<script src="js/footer_scripts.js"></script>
+	</body>
+</html>
---
+++ public/php/variables.php
@@ -0,0 +1,18 @@
+<?php 
+// php document
+// author: derek moore
+
+
+// global variables
+
+	// includes
+	$server = $_SERVER['DOCUMENT_ROOT'];
+	
+	$inc_css = $server."/(!!!)/(!!!).inc";
+	
+	$inc_head = $server."/(!!!)/(!!!).inc";
+	$inc_header = $server."/(!!!)/(!!!).inc";
+	$inc_nav = $server."/(!!!)/(!!!).inc";
+	$inc_footer = $server."/(!!!)/(!!!).inc";
+
+?>
