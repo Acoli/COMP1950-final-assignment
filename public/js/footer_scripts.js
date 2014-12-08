df0b067e5379b8a433485a9bdefea79861d2ce67
Derek <fiveflip@gmail.com>
2014-12-07 21:15:14 -0800

    bunch of changes, including ajax and nav changes

--- public/js/footer_scripts.js
+++ public/js/footer_scripts.js
@@ -13,10 +13,16 @@ code manipulation
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 DOM manipulation
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+ // global variables
+ //var site = "http://bcitcomp.ca/students/syonggarrido/assignments/";
+ var site = "http://localhost"
  
 	// AJAX
-	$('a[href="#"]').click(function(){
+	$('div#pageslide ul li a, nav a').click(function(e){
+		e.preventDefault();
+		console.log("prevented default");
 		var target = $(this).attr('data-target');
 		console.log(target);
 		$('div.content').load(target);
+		//window.history.pushState("","",site+"/"+target);
 	})
