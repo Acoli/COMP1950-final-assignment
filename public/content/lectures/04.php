206ee1f777e42110aa8b3c3bcfa17a3ecaf1cdc9
fiveflip <fiveflip@gmail.com>
2014-12-08 20:19:59 -0800

    removed ajax, halfway through page restructuring

--- public/content/lectures/04.php
+++ public/content/lectures/04.php
@@ -6,26 +6,26 @@
 	page: index.php
 	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 	<head>
-		<title>comp 1950 | bcit</title>
-		<?php include 'includes/head.inc';?>
+		<title>lecture 4 | comp 1950 | bcit</title>
+		<?php include $_SERVER['DOCUMENT_ROOT'] . '/comp1950-final/public/includes/head.inc';?>
 	</head>
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
 	<body class="bac3">
 		<div id="wrapper_body">
 			<header class="bac1">
-				<?php include 'includes/header.inc';?>
+				<?php include $_SERVER['DOCUMENT_ROOT'] . '/comp1950-final/public/includes/header.inc';?>
 			</header>
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->
 			<nav class="bac1">	
-				<?php include 'includes/nav.inc';?>
+				<?php include $_SERVER['DOCUMENT_ROOT'] . '/comp1950-final/public/includes/nav.inc';?>
 			</nav>
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->		
 			<main class="bac1">
-				<?php include 'includes/main.inc';?>
+				<?php include $_SERVER['DOCUMENT_ROOT'] . '/comp1950-final/public/includes/lectures/04.inc';?>
 			</main>
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->		
 			<footer>
-				<?php include 'includes/footer.inc';?>
+				<?php include $_SERVER['DOCUMENT_ROOT'] . '/comp1950-final/public/includes/footer.inc';?>
 			</footer>
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  -->			
 		</div>
@@ -34,8 +34,8 @@
 		<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-		<script src="js/footer_scripts.js"></script>
-		<script src="js/jquery.pageslide.min.js"></script>
-		<script src="js/functions.js"></script>
+		<script src="/comp1950-final/public/js/footer_scripts.js"></script>
+		<script src="/comp1950-final/public/js/jquery.pageslide.min.js"></script>
+		<script src="/comp1950-final/public/js/functions.js"></script>
 	</body>
 </html>
--- public/js/footer_scripts.js
+++ public/js/footer_scripts.js
@@ -15,14 +15,14 @@ DOM manipulation
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
  // global variables
  //var site = "http://bcitcomp.ca/students/syonggarrido/assignments/";
- var site = "http://localhost"
+ var site = "http://localhost";
  
 	// AJAX
-	$('div#pageslide ul li a, nav a').click(function(e){
+	/* $('div#pageslide ul li a, nav a').click(function(e){
 		e.preventDefault();
 		console.log("prevented default");
 		var target = $(this).attr('data-target');
 		console.log(target);
 		$('div.content').load(target);
 		//window.history.pushState("","",site+"/"+target);
-	})
+	}) */
